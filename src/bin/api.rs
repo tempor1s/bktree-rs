1ee3dea58d17afbf7dd7b2e173e7a7646f292ffb
Ben <benlaugherty@gmail.com>
2020-05-02 16:52:25 -0600

    respond with json instead of plain text :)

--- src/bin/api.rs
+++ src/bin/api.rs
@@ -4,19 +4,44 @@
 extern crate rocket;
 
 use bktree_rs::*;
-use rocket::response::status;
 use rocket::State;
+use rocket_contrib::json::Json;
+use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::{BufRead, BufReader, Error};
 
+#[derive(Debug, Deserialize, Serialize)]
+struct ExactCloseResponse {
+    exact: Vec<String>,
+    close: Vec<String>,
+}
+
+impl<'a> ExactCloseResponse {
+    fn new(exact: Vec<String>, close: Vec<String>) -> Self {
+        ExactCloseResponse { exact, close }
+    }
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "Welcome to the 'did you mean' api."
+}
+
 #[get("/<word>/<tolerence>")]
-fn index(
+fn get_word(
     word: String,
     tolerence: usize,
     bktree: State<SpellTree<String>>,
-) -> status::Accepted<String> {
-    let (_exact, close) = bktree.find(&word, tolerence);
-    status::Accepted(Some(format!("{:?}", close)))
+) -> Json<ExactCloseResponse> {
+    // find the item in the tree
+    let (exact, close) = bktree.find(&word, tolerence);
+
+    // convert &Strings to a normal String
+    let exact: Vec<String> = exact.iter().map(|val| val.to_string()).collect();
+    let close: Vec<String> = close.iter().map(|val| val.to_string()).collect();
+
+    // return the json response
+    Json(ExactCloseResponse::new(exact, close))
 }
 
 fn generate_tree(filename: &str) -> SpellTree<String> {
@@ -42,6 +67,6 @@ fn main() {
     let tree: SpellTree<String> = generate_tree("words.txt");
     rocket::ignite()
         .manage(tree)
-        .mount("/", routes![index])
+        .mount("/", routes![index, get_word])
         .launch();
 }
